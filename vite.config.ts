3531218bf572310187e9f51e4a21fe11878430b3
Hita Thota <hita@thethotas.com>
2025-11-25 19:44:13 -0800

    modify config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: true,
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
